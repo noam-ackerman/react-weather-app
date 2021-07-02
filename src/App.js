ed04db6b21d23ed77f1442a57f6b37581eb90722
noam-ackerman <noamac1812@gmail.com>
2021-07-02 12:20:14 +0200

    installing bootstrap and adjusting

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import Forecast from "./Forecast";
 
 function App() {
   return (
-    <div>
-      <div className="Box">
-        <SearchCity />
-        <Forecast />
+    <div className="App">
+      <div className="container">
+        <div className="Box">
+          <SearchCity />
+          <Forecast />
+        </div>
+        <p className="sourceLine">
+          <a
+            href="https://github.com/noam-ackerman/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            className="codeLink"
+          >
+            Open-source code
+          </a>{" "}
+          designed and built by{" "}
+          <a
+            href="https://www.linkedin.com/in/noam-ackerman/"
+            target="_blank"
+            rel="noreferrer"
+            className="codeLink"
+          >
+            Noam Ackerman
+          </a>
+        </p>
       </div>
-      <p className="sourceLine">
-        <a
-          href="https://github.com/noam-ackerman/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-          className="codeLink"
-        >
-          Open-source code
-        </a>{" "}
-        designed and built by{" "}
-        <a
-          href="https://www.linkedin.com/in/noam-ackerman/"
-          target="_blank"
-          rel="noreferrer"
-          className="codeLink"
-        >
-          Noam Ackerman
-        </a>
-      </p>
     </div>
   );
 }
--- src/DayForecast.js
+++ src/DayForecast.js
@@ -4,7 +4,7 @@ import "./Forecast.css";
 
 export default function DayForecast(props) {
   return (
-    <div className="col-2 dayForecastSection">
+    <div className="col-2">
       <div className="day">{props.day}</div>
       <img src={props.icon} alt="partlySunny" className="forecastIcon" />
       <div className="forecastTemp">
